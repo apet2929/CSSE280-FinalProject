331d136817d7b2f310cf6a5e3958e7eab1af416e
Michael Chalupiak <chalupmc@rose-hulman.edu>
2023-10-25 09:28:34 -0400

    searchbar

--- frontend/csse280-final-project/src/PageHeader.tsx
+++ frontend/csse280-final-project/src/PageHeader.tsx
@@ -1,6 +1,12 @@
 const PageHeader = () => (
     <div id="header" className="position-absolute">
-      <h3 className='align-middle'>Hello World</h3>
+        {/* <h3 className='align-middle'>Hello World</h3> */}
+      <img src="https://in211.communityos.org/publicassets/logo.png" id="navlogo" />
+      {/* <input type="search" className="w-75 mx-4" placeholder="Search for a Service"/> */}
+      <div className="input-group mb-3 w-75 d-inline">
+        <input type="search" id="searchbar" className="form-control d-inline" placeholder="Search for a Service" aria-label="Search for a service" aria-describedby="searchbutton" />
+        <button className="btn btn-outline-primary" type="button" id="searchbutton">Search</button>
+      </div>
     </div>
 )
 
