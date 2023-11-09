67ed33e0b2dd08f4d663a8f333c1c41da88f3980
Michael Chalupiak <chalupmc@rose-hulman.edu>
2023-11-09 14:18:05 -0500

    updated styling

--- frontend/csse280-final-project/src/ServiceCategoryPopup.tsx
+++ frontend/csse280-final-project/src/ServiceCategoryPopup.tsx
@@ -6,9 +6,10 @@ type CategoryProps = {
 }
 
 let Category = (props: CategoryProps) => {
-    return <li>
-        <button type="button" className="btn btn-primary w-25" onClick={(e) => props.onClick()}>{ props.name }</button>
-    </li>
+    // return <li>
+    //     <button type="button" className="btn btn-primary w-25" onClick={(e) => props.onClick(props.name)}>{ props.name }</button>
+    // </li>
+    return <button type="button" className="btn btn-primary mx-4 my-2" onClick={(e) => props.onClick(props.name)}>{ props.name }</button>
 }
 
 type PopupProps = {
@@ -17,32 +18,22 @@ type PopupProps = {
 }
 
 let ServiceCategoryPopup = (props: PopupProps) => {
-    return <div id="serviceCategoryPopup" className="container">
-        <div className="card">
+    return <div id="serviceCategoryPopup" className="container p-0">
+        <div className="card categoryCard">
             <div className="card-header">
             Choose from the categories below and browse programs local to {props.countyName}
             </div>
-            <div className="card-body container">
-                <div className="row">
-                    <div className="col-6">
-                        <ul className="list-unstyled">
-                            <Category name="star" onClick={() => props.onClick("star")}/>
-                            <Category name="star" onClick={() => props.onClick("star1")}/>
-                            <Category name="star" onClick={() => props.onClick("star2")}/>
-                            <Category name="star" onClick={() => props.onClick("star3")}/>
-                        </ul>
-                    </div>
-                    <div className="col-6">
-                        <ul className="list-unstyled">
-                            <li>Hi 2</li>
-                            <li>Hi 2</li>
-                            <li>Hi 2</li>
-                            <li>Hi 2</li>
-                        </ul>
-                    </div>
-                </div>
-                
-                
+            <div className="card-body text-center container">
+                    <Category name="Basic Needs" onClick={props.onClick}/>
+                    <Category name="Consumer Services" onClick={props.onClick}/>
+                    <Category name="Criminal Justice and Legal Services" onClick={props.onClick}/>
+                    <Category name="Education" onClick={props.onClick}/>
+                    <Category name="Environment and Public Health/Safety" onClick={props.onClick}/>
+                    <Category name="Health Care" onClick={props.onClick}/>
+                    <Category name="Income Support and Employment" onClick={props.onClick}/>
+                    <Category name="Individual and Family Life" onClick={props.onClick}/>
+                    <Category name="Mental Health and Substance Use Disorder Services" onClick={props.onClick}/>
+                    <Category name="Organizational/Community/International Services" onClick={props.onClick}/>
             </div>
         </div>
     </div>
